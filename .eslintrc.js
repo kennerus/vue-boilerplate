e460515f3ddd1ab114096b57c3d00e2056088b32
kennerus <kennerus@gmail.com>
2020-03-30 18:25:03 +0300

    ## init commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,35 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-};
+	root: true,
+	env: {
+		es6: true,
+		node: true
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:vue/recommended",
+		"prettier/vue"
+	],
+	parserOptions: {
+		parser: "babel-eslint",
+		sourceType: "module"
+	},
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		"indent": ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		"quotes": ["error", "double"],
+		"semi": ["error", "never"]
+	},
+	overrides: [
+		{
+			files: [
+				"**/__tests__/*.{j,t}s",
+				"**/tests/unit/**/*.spec.{j,t}s"
+			],
+			env: {
+				jest: true
+			}
+		}
+	]
+}
